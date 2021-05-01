cbbf1d19d6a917e0c781a200ad659d710e33bb8a
Plinio Muniz <pliniomuniz2016@gmail.com>
2021-05-01 14:42:30 -0300

    Create MUNIZ-1.4.cpp

---
+++ FOURTH/MUNIZ-1.4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    float a, b, a1,b1, d1, r3;
+
+cout << "\nQuanto você ganha por hora?" << endl;
+cin >> a;
+cout << "\nQuantas horas voce trabalha por mês?" << endl;
+cin >> b;  
+
+a1 = a * b;
+b1 = a1 - 11
+
+
+r1 = n1 * 2 * (n2 / 2);
+r2 = (n1 *  3) + d1;
+r3 = d1*d1*d1;
+
+
+cout << "Salario bruto do mês"<< a1 << endl;
+cout << "A soma do triplo do primeiro com o terceiro: "<< r2 << endl;
+cout << "O terceiro elevado ao cubo: "<< r3 << endl;
+
+        return(0);
+}
