c0b0d795111cc4f30681e9e06ae8ca6dab1530d8
Plinio Muniz <pliniomuniz2016@gmail.com>
2021-05-03 07:07:14 -0300

    up
    a

--- FOURTH/MUNIZ-2.1.4.cpp
+++ FOURTH/MUNIZ-2.1.4.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main(){
 
-    cout << "Eae Prof" << endl;
+    int main(){
+
+    float a, b, c, d;
+
+cout << "\nQual o tamanho em metros da sua area quadrada?" << endl;
+cin >> a;
+/*areaquadrada*/
+
+b = a / 3;
+/*Quantidade de Litros*/
+c = ceil( b / 18 );
+/*Latas*/
+d = ceil( c * 80);
+/*Valor a ser pago*/ 
+
+
+cout << "Litros de tintas necessarios = "<< b << endl;
+cout << "Latas = "<< c << endl;
+cout << "Valor total a ser pago = "<< d << endl;
     
     return 0;
     
